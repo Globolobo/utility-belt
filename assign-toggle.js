52896c9c57e3352984d44fb70f3e177567e553e2
Sean <Globolobo@users.noreply.github.com>
2018-03-08 12:53:33 +1000

    Update assign-toggle.js

--- assign-toggle.js
+++ assign-toggle.js
@@ -1,18 +1,13 @@
 import _ from 'lodash';
-// Accepts - 1 source as a string, or multiple sources as an array.
-// Returns - sources as key, and target as !target in a {key:!target} format.
+// Accepts: 1 source as a string, or multiple sources as an array.
+// Returns: sources as key, and target as !target in a {key:!target} format.
 
-const AssignToggle = (target, sources) => {
-  let toggledObject;
-  if (Array.isArray(sources)) {
-    toggledObject = _.assign({}, target,
-      ...sources.map((key) => {
-        return { [key]: !target[key] };
-      })
-    );
-  } else {
-    toggledObject = _.assign({}, target, { [sources]: !target[sources] });
-  }
-  return toggledObject;
-};
+const AssignToggle = (target, sources) => 
+  ({
+    _.assign({}, target, Array.isArray(sources) ? 
+        ...sources.map((key) => ({ [key]: !target[key] });
+        :
+        { [sources]: !target[sources] });
+  })
+
 export default AssignToggle;
